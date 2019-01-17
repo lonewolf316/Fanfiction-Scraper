42a8b18a95dba93f0996fea289928cf4c6135130
Sagan <saganm316@gmail.com>
2019-01-16 20:39:40 -0500

    creates list of all sids

--- ffscraper.py
+++ ffscraper.py
@@ -31,14 +31,11 @@ def get_sids_from_catagory(crossover,medium,ffcatagory):
     webpage = str("http://fanfiction.net/") + webpageext
 
     #get page 1 of fanfiction and import to bs4 for processing
-    print("Retrieving webpage:", webpage)
     start = time.time()
     session = requests.Session()
     session.trust_env = False 
     r = session.get(webpage)
-    print("Retrieved webpage. Scanning for SIDs now.")
     end = time.time()
-    print("This took:", end-start, "seconds")
     soup = bs4.BeautifulSoup(r.content, "html.parser")
 
     #loop to find link of last page
@@ -49,15 +46,35 @@ def get_sids_from_catagory(crossover,medium,ffcatagory):
             templinklist.append(fullurl)
     lastpage = templinklist[-2]
     lastpagenum = lastpage[(lastpage.find("&pe")-2):]
+    currentpage = 1
+    webpage=webpage+"/?&srt=1&r=103&p="
+    sidlist=[]
 
-    #iterate through links on first page to collect SIDs
-    for a in soup.find_all('a', href=True):
-        fullurl = str(a['href'])
-        if fullurl.startswith("/s/"):
-            slicedurl = fullurl[3:]
-            endofsid = slicedurl.find('/')
-            sid = slicedurl[:endofsid]
-            print(sid)
-    pagenum=2
+    #Loop for each page until last page and get SIDs on each page
+    while currentpage <= int(lastpagenum):
+        print("Waiting 1 second to prevent spam")
+        time.sleep(1)
+        downloadpage = webpage + str(currentpage)
+        print("Downloading and parsing page:", downloadpage)
+        r = session.get(downloadpage)
+        soup = bs4.BeautifulSoup(r.content, "html.parser")
+        pagesidlist=[]
 
+    #iterate through links on page to collect SIDs
+        for a in soup.find_all('a', href=True):
+            fullurl = str(a['href'])
+            if fullurl.startswith("/s/"):
+                slicedurl = fullurl[3:]
+                endofsid = slicedurl.find('/')
+                sid = slicedurl[:endofsid]
+                pagesidlist.append(sid)
+        print("Found", len(pagesidlist), "SIDs on page")
+
+        for sid1 in pagesidlist:
+            if sid1 not in sidlist:
+                sidlist.append(sid1)
+
+        currentpage+=1
+    print(sidlist)
+    print(len(sidlist))
 get_sids_from_catagory(False, "movie", "Zootopia")
