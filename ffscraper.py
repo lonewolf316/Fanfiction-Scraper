7d4909ab3da5253b499309464d7cc4bd8be46011
thelonewolf316 <thelonewolf316>
2019-01-17 22:40:07 -0500

    now can detect and download SIDs if only one page of links per canon

--- ffscraper.py
+++ ffscraper.py
@@ -1,8 +1,9 @@
 import time, os, fanfiction, requests, bs4
-if not os.path.exists("stories"):
-    os.makedirs("stories")
+
 
 def get_fanfic_text(sid):
+    if not os.path.exists("stories"):
+        os.makedirs("stories")
     scraper = fanfiction.Scraper()
     metadata = scraper.scrape_story_metadata(sid)
     title = metadata.get("title")
@@ -25,11 +26,11 @@ def get_fanfic_text(sid):
 def get_sids_from_catagory(crossover,medium,ffcatagory):
     #create extensions for url based on input, create directory for saving, and craft url to scrape
     if crossover:
-        webpageext = "crossovers"+medium+"/" 
+        webpageext = "crossovers/" + medium + "/"
     else:
-        webpageext = medium+"/"
-    if not os.path.exists(webpageext):
-        os.makedirs(webpageext)
+        webpageext = medium + "/"
+    if not os.path.exists("sids/"+webpageext):
+        os.makedirs("sids/"+webpageext)
     webpage = str("http://fanfiction.net/") + webpageext + ffcatagory
 
     #get page 1 of fanfiction and import to bs4 for processing
@@ -39,50 +40,72 @@ def get_sids_from_catagory(crossover,medium,ffcatagory):
 
     soup = bs4.BeautifulSoup(r.content, "html.parser")
 
+    singlepage = True
     #loop to find link of last page
     templinklist=[]
     for a in soup.find_all('a', href=True):
         fullurl = str(a['href'])
         if fullurl.startswith("/"+webpageext):
             templinklist.append(fullurl)
-    lastpage = templinklist[-2]
-    lastpagenum = lastpage[(lastpage.find("&pe")-2):]
-    currentpage = 1
-    webpage=webpage+"/?&srt=1&r=103&p="
+        if "&p=" in fullurl:
+            
+            singlepage = False
+    
     sidlist=[]
-
-    #Loop for each page until last page and get SIDs on each page
-    while currentpage <= int(lastpagenum):
-        print("Waiting 1 second to prevent spam")
+    if singlepage:
+        print("This has only one page")
         time.sleep(1)
-        downloadpage = webpage + str(currentpage)
+        downloadpage = webpage
         print("Downloading and parsing page:", downloadpage)
         r = session.get(downloadpage)
         soup = bs4.BeautifulSoup(r.content, "html.parser")
         pagesidlist=[]
-
-    #iterate through links on page to collect SIDs
         for a in soup.find_all('a', href=True):
             fullurl = str(a['href'])
             if fullurl.startswith("/s/"):
                 slicedurl = fullurl[3:]
                 endofsid = slicedurl.find('/')
                 sid = slicedurl[:endofsid]
-                pagesidlist.append(sid)
-        print("Found", len(pagesidlist), "SIDs on page")
-
-        for sid in pagesidlist:
-            if sid not in sidlist:
                 sidlist.append(sid)
-        currentpage+=1
+
+    else:
+        print("This canon has multiple pages")
+        lastpage = templinklist[-2]
+        lastpagenum = lastpage[(len(lastpage))-lastpage.find("&pe")*-1:]
+        currentpage = 1
+        webpage=webpage+"/?&srt=1&r=103&p="
+        #Loop for each page until last page and get SIDs on each page
+        while currentpage <= int(lastpagenum):
+            print("Waiting 1 second to prevent spam")
+            time.sleep(1)
+            downloadpage = webpage + str(currentpage)
+            print("Downloading and parsing page:", downloadpage)
+            r = session.get(downloadpage)
+            soup = bs4.BeautifulSoup(r.content, "html.parser")
+            pagesidlist=[]
+
+        #iterate through links on page to collect SIDs
+            for a in soup.find_all('a', href=True):
+                fullurl = str(a['href'])
+                if fullurl.startswith("/s/"):
+                    slicedurl = fullurl[3:]
+                    endofsid = slicedurl.find('/')
+                    sid = slicedurl[:endofsid]
+                    pagesidlist.append(sid)
+            print("Found", len(pagesidlist), "SIDs on page")
+
+            for sid in pagesidlist:
+                if sid not in sidlist:
+                    sidlist.append(sid)
+            currentpage+=1
 
     #Save into file    
     savedir = "sids/"+webpageext+ffcatagory+".txt"
     print("Saving:", len(sidlist), "SIDs to", savedir)
     with open(savedir, "w") as f:
         for sid in sidlist:
-            f.write(sid+"/n")
+            f.write(sid+"\n")
         f.close()
     print("Done")
 
-get_sids_from_catagory(False, "movie", "Zootopia")
+get_sids_from_catagory(False, "tv", "Shield")
